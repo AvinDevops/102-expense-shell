b9fc2b200bd8ff37dcfff70d97e0e95c12df8dce
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 18:27:13 +0530

    configuring db

---
+++ db-mysql.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USER=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+ if [ $1 -ne 0 ]
+ then
+    echo -e "$2 is...$R FAILED $N"
+    exit
+ else
+    echo -e "$2 is...$G SUCCESS $N"
+ fi   
+}
+
+if [ $USER -ne 0 ]
+then
+    echo -e "$R you are not root user, please access with root $N"
+    exit
+else
+    echo -e "$G you are root user $N"
+fi
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabiling mysql"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting mysql"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting password for root"
